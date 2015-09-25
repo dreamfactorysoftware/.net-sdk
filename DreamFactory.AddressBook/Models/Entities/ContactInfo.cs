e54d331348ab3eda83e8545e733023f4261ec4fc
Marko Kosović <marko.kosovic@toptal.com>
2015-09-25 11:24:47 +0200

    Added ContactInfo field validations.

--- DreamFactory.AddressBook/Models/Entities/ContactInfo.cs
+++ DreamFactory.AddressBook/Models/Entities/ContactInfo.cs
@@ -16,30 +16,37 @@
         public string InfoType { get; set; }
 
         [Display(Name = "Phone")]
+        [Required(ErrorMessageResourceType = typeof(Resources), ErrorMessageResourceName = "ErrorMessage_Required", ErrorMessage = null)]
         [MaxLength(32, ErrorMessageResourceType = typeof(Resources), ErrorMessageResourceName = "ErrorMessage_MaxLength", ErrorMessage = null)]
         public string Phone { get; set; }
 
         [Display(Name = "Email")]
+        [Required(ErrorMessageResourceType = typeof(Resources), ErrorMessageResourceName = "ErrorMessage_Required", ErrorMessage = null)]
         [MaxLength(255, ErrorMessageResourceType = typeof(Resources), ErrorMessageResourceName = "ErrorMessage_MaxLength", ErrorMessage = null)]
         public string Email { get; set; }
 
         [Display(Name = "Address")]
+        [Required(ErrorMessageResourceType = typeof(Resources), ErrorMessageResourceName = "ErrorMessage_Required", ErrorMessage = null)]
         [MaxLength(255, ErrorMessageResourceType = typeof(Resources), ErrorMessageResourceName = "ErrorMessage_MaxLength", ErrorMessage = null)]
         public string Address { get; set; }
 
         [Display(Name = "City")]
+        [Required(ErrorMessageResourceType = typeof(Resources), ErrorMessageResourceName = "ErrorMessage_Required", ErrorMessage = null)]
         [MaxLength(64, ErrorMessageResourceType = typeof(Resources), ErrorMessageResourceName = "ErrorMessage_MaxLength", ErrorMessage = null)]
         public string City { get; set; }
 
         [Display(Name = "State")]
+        [Required(ErrorMessageResourceType = typeof(Resources), ErrorMessageResourceName = "ErrorMessage_Required", ErrorMessage = null)]
         [MaxLength(64, ErrorMessageResourceType = typeof(Resources), ErrorMessageResourceName = "ErrorMessage_MaxLength", ErrorMessage = null)]
         public string State { get; set; }
 
         [Display(Name = "Zip")]
+        [Required(ErrorMessageResourceType = typeof(Resources), ErrorMessageResourceName = "ErrorMessage_Required", ErrorMessage = null)]
         [MaxLength(16, ErrorMessageResourceType = typeof(Resources), ErrorMessageResourceName = "ErrorMessage_MaxLength", ErrorMessage = null)]
         public string Zip { get; set; }
 
         [Display(Name = "Country")]
+        [Required(ErrorMessageResourceType = typeof(Resources), ErrorMessageResourceName = "ErrorMessage_Required", ErrorMessage = null)]
         [MaxLength(64, ErrorMessageResourceType = typeof(Resources), ErrorMessageResourceName = "ErrorMessage_MaxLength", ErrorMessage = null)]
         public string Country { get; set; }
 
